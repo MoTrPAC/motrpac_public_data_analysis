360718fe7b40b284840276e5b086444fb204ddd5
david-dd-amar <David@CVMED-C02TG17GG8WL.local>
2017-08-17 14:36:25 -0700

    metafor analysis was added

--- dataset_level_meta_analysis.R
+++ dataset_level_meta_analysis.R
@@ -84,6 +84,7 @@ for(i in 2:length(acute_datasets)){
 }
 acute_mod = get_dataset_moderators(names(acute_datasets_effects),acute_metadata)
 acute_gene_tables = lapply(acute_genes,get_gene_table,dataset_effects=acute_datasets_effects,moderators=acute_mod)
+names(acute_gene_tables) = acute_genes
 longterm_genes = rownames(longterm_datasets[[1]]$gene_data)
 for(i in 2:length(longterm_datasets)){
   if(length(acute_gene_tables)==0){next}
@@ -91,35 +92,69 @@ for(i in 2:length(longterm_datasets)){
 }
 longterm_mod = get_dataset_moderators(names(longterm_datasets_effects),longterm_metadata)
 longterm_gene_tables = lapply(longterm_genes,get_gene_table,dataset_effects=longterm_datasets_effects,moderators=longterm_mod)
+names(longterm_gene_tables) = longterm_genes
 
-get_gene_analysis_pvals<-function(gdata){
-  v = rep(NA,20)
+get_gene_analysis_pvals<-function(gdata,use_mods=T,func=rma,...){
+  gdata = gdata[gdata$vi>0,]
+  v = NULL
   try({
-  res1 = rma(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, subset = (tissue=="blood"))
-  ps1 = res1$pval;names(ps1) = rownames(res1$b)
-  ps1 = c(ps1,res1$QMp);names(ps1)[length(ps1)] = "AllMods"
-  stats1 = res1$b[,1];ses1 = res1$se
-  res2 = rma(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, subset = (tissue=="muscle"))
-  ps2 = res2$pval;names(ps2) = rownames(res2$b)
-  stats2 = res2$b[,1];ses2 = res2$se
-  ps2 = c(ps2,res2$QMp);names(ps2)[length(ps2)] = "AllMods"
-  names(ps1) = paste("blood_",names(ps1),sep="")
-  names(ps2) = paste("muscle_",names(ps2),sep="")
-  names(stats1) = paste("blood_",names(stats1),sep="")
-  names(stats2) = paste("muscle_",names(stats2),sep="")
-  v = c(ps1,ps2,stats1,stats2)
+    if(use_mods){
+      res1 = func(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, 
+                 subset = (tissue=="blood"),control=list(maxiter=10000,stepadj=0.5),...) 
+      res2 = func(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, 
+                 subset = (tissue=="muscle"),control=list(maxiter=10000,stepadj=0.5),...)
+    }
+    else{
+      res1 = func(yi=yi,vi=vi,data=gdata,knha=T, subset = (tissue=="blood"),control=list(maxiter=10000,stepadj=0.5),...) 
+      res2 = func(yi=yi,vi=vi,data=gdata,knha=T, subset = (tissue=="muscle"),control=list(maxiter=10000,stepadj=0.5),...)
+    }
+    ps1 = res1$pval;names(ps1) = rownames(res1$b)
+    ps1 = c(ps1,res1$QMp);names(ps1)[length(ps1)] = "AllMods"
+    stats1 = res1$b[,1];ses1 = res1$se
+    ps2 = res2$pval;names(ps2) = rownames(res2$b)
+    stats2 = res2$b[,1];ses2 = res2$se
+    ps2 = c(ps2,res2$QMp);names(ps2)[length(ps2)] = "AllMods"
+    names(ps1) = paste("blood_p_",names(ps1),sep="")
+    names(ps2) = paste("muscle_p_",names(ps2),sep="")
+    names(stats1) = paste("blood_s_",names(stats1),sep="")
+    names(stats2) = paste("muscle_s_",names(stats2),sep="")
+    v = c(ps1,ps2,stats1,stats2)
   })
   return(v)
 }
-acute_meta_analysis_res = sapply(acute_gene_tables,get_gene_analysis_pvals)
-longterm_meta_analysis_res = sapply(longterm_gene_tables,get_gene_analysis_pvals)
+acute_meta_analysis_res = t(sapply(acute_gene_tables,get_gene_analysis_pvals,use_mods=F))
+longterm_meta_analysis_res = t(sapply(longterm_gene_tables,get_gene_analysis_pvals,use_mods=F))
+# longterm had several fails, the above line returns a list
+names(longterm_meta_analysis_res) = names(longterm_gene_tables)
+longterm_meta_analysis_res = longterm_meta_analysis_res[!sapply(longterm_meta_analysis_res,is.null)]
+tmp_mat = t(sapply(longterm_meta_analysis_res,function(x) x ))
+longterm_meta_analysis_res = tmp_mat
 save(acute_meta_analysis_res,longterm_meta_analysis_res,file="PADB_metafor_meta_analysis_results.RData")
 
+par(mfrow=c(2,2))
+hist(acute_meta_analysis_res[,"blood_p_intrcpt"])
+hist(longterm_meta_analysis_res[,"blood_p_intrcpt"])
+hist(acute_meta_analysis_res[,"muscle_p_intrcpt"])
+hist(longterm_meta_analysis_res[,"muscle_p_intrcpt"])
+
+metafor_gene_sets = list()
+metafor_gene_sets[["acute_muscle"]] = names(which(p.adjust(acute_meta_analysis_res[,"muscle_p_intrcpt"],method='fdr')<0.1))
+metafor_gene_sets[["acute_blood"]] = names(which(p.adjust(acute_meta_analysis_res[,"blood_p_intrcpt"],method='fdr')<0.1))
+metafor_gene_sets[["longterm_muscle"]] = names(which(p.adjust(longterm_meta_analysis_res[,"muscle_p_intrcpt"],method='fdr')<0.1))
+metafor_gene_sets[["longterm_blood"]] = names(which(p.adjust(longterm_meta_analysis_res[,"blood_p_intrcpt"],method='fdr')<0.1))
+metafor_gene_sets_enrichments = run_topgo_enrichment_fisher(metafor_gene_sets,union(acute_genes,longterm_genes))
+
 # tests and comments from the paper of metafor (2010)
-gdata = get_gene_table("1282",acute_datasets_effects,acute_mod)
+gdata = get_gene_table("8863",acute_datasets_effects,acute_mod)
+gdata = get_gene_table("65121",longterm_datasets_effects,longterm_mod)
+# another test: make a dataset with large effects
+# gdata$yi = rnorm(nrow(gdata),mean=5)
 plot(gdata[,"yi"],gdata[,"vi"],pch=as.numeric(as.factor(gdata$tissue)))
 # knha - a correction that accounts for the uncertainty in the random effect
 res0 = rma(yi,vi,data=gdata,knha=T)
+res0 = rma.mv(yi,vi,random = ~ 1|gse,data=gdata)
+res0 = rma(yi,vi,data=gdata,knha=T,subset = (tissue=="muscle"))
+res0 = rma.mv(yi,vi,random = ~ 1|gse,data=gdata,subset = (tissue=="muscle"))
 # explanation of the result above:
 #   mu - the average effect is 0.067, the CI contains zero
 res = rma(yi=yi,vi=vi,mods = ~ tissue + training + time ,data=gdata,knha=T)
@@ -142,7 +177,7 @@ predict(res)
 res2 = rma(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, subset = (tissue=="muscle"))
 res1 = rma(yi=yi,vi=vi,mods = ~ training + time ,data=gdata,knha=T, subset = (tissue=="blood"))
 # Residual analysis for detecting outlier datasets
-barplot(as.numeric(rstudent(res2)$z));abline(-2,0);abline(2,0)
+barplot(as.numeric(rstudent(res)$z));abline(-2,0);abline(2,0)
 # Residual analysis is informative but not enough
 # case deletion diagnostics are informatice as well
 plot(influence(res))
