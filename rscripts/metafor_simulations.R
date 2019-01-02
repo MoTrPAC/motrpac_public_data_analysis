273c6e6586b0464acde32b5de7595c6e95e75ca2
david-dd-amar <David@CVMED-C02TG17GG8WL.local>
2019-01-02 15:29:34 -0800

    reordering material

--- rscripts/metafor_simulations.R
+++
@@ -1,9 +0,0 @@
-# Meta analysis simulations
-ncohorts = 10
-nstudies = 5
-yi = rnorm(ncohorts)
-vi = rep(1,ncohorts)
-S = rep(1:nstudies,(ncohorts/nstudies))
-yi[S==1] = yi[S==1]+10
-library(metafor)
-rma.mv(yi,vi,random = ~1|S, mods = S)
